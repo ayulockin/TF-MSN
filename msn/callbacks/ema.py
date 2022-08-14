f6adec95d4c5a39a6198e8828974c4bb406adfd7
ayulockin <mein2work@gmail.com>
2022-08-14 05:50:49 +0000

    type annotation + documentation

--- msn/callbacks/ema.py
+++ msn/callbacks/ema.py
@@ -3,7 +3,27 @@ import wandb
 
 
 class EMA(tf.keras.callbacks.Callback):
-    def __init__(self, decay=0.999):
+    """Use this class to update the weights of a target model
+    with exponential moving average of the weights of an anchor
+    model. 
+
+    The model should have the structure:
+        ```
+        def siamese_network():
+            inputs = layers.Input(shape=(...))
+            # Init anchor model
+            anchor_model = build_anchor_model(name='anchor_model')
+            # Init target model without trainable params.
+            target_model = build_target_model(name='target_model')
+            target_model.trainable = False
+            ...
+            return models.Model(inputs, outputs=[...])
+        ```
+
+    Args:
+        decay (float): The decay/momentum parameter.
+    """
+    def __init__(self, decay: float=0.999):
         super(EMA, self).__init__()
         # TODO (ayulockin): make decay a tf.Variable instance and increase it
         # linearly as the training progesses.
@@ -12,11 +32,11 @@ class EMA(tf.keras.callbacks.Callback):
         # Create an ExponentialMovingAverage object
         self.ema = tf.train.ExponentialMovingAverage(decay=self.decay_var)
 
-    def on_train_begin(self, logs=None):
+    def on_train_begin(self, logs: dict=None) -> None:
         # TODO (ayulockin): add assertion for anchor_model and target_model.
         self.ema.apply(self.model.get_layer('anchor_model').trainable_variables)
 
-    def on_train_batch_end(self, batch, logs=None):
+    def on_train_batch_end(self, batch: int, logs: dict=None) -> None:
         # Get exponential moving average of anchor model weights.
         train_vars = self.model.get_layer('anchor_model').trainable_variables
         averages = [self.ema.average(var) for var in train_vars]
